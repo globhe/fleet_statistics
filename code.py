6356517b2a0a591960ac7deb67a4c8980812b1f7
Rémy Ghalayini <63195370+rghalayini@users.noreply.github.com>
2022-03-25 07:25:57 +0100

    added first draft of python file

---
+++ code.py
@@ -0,0 +1,16 @@
+import pandas as pd
+pd.set_option('display.max_rows', None)
+file = pd.read_csv('drones.csv', sep=';', skipinitialspace = True, header=None)
+
+#define a dataframe
+df = pd.DataFrame(file)
+
+# make lower case
+df = df.astype(str).apply(lambda x: x.str.lower())
+
+#filter bad data
+filtered=df.replace({'phatom': 'phantom', '2.0': 'NaN', '1.0': 'NaN', 'professional': 'pro'}, regex=True)
+
+#count all variables
+count_drones=df.stack().value_counts()
+print(count_drones)
